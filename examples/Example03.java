f99d29df5eb97a52243526ea64284acfcd4c67e3
7978606023 <anubhavwhitebox@gmail.com>
2020-05-18 03:56:43 -0700

    first prog

---
+++ examples/Example03.java
@@ -0,0 +1,14 @@
+package com.ip.examples;
+
+import java.util.stream.Stream;
+
+
+public class Example03 {
+
+    public static void main(String[] args) {
+
+        int number = Stream.iterate(1, n -> n * 2)
+                        .filter(n -> n % 1024 == 0)
+                        .findFirst().get();
+    }
+}
---
+++ examples/Example04.java
@@ -0,0 +1,19 @@
+package com.ip.examples;
+
+import java.util.stream.IntStream;
+
+public class Example04 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+        }
+
+        int easySum = IntStream.range(0, 100)
+                .sum();
+
+        System.out.println("Sum with for-loop:" + sum);
+        System.out.println("Sum with int stream:" + easySum);
+    }
+}
